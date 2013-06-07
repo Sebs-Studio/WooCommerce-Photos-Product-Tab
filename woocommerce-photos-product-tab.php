792cbcfc9cf508cae578879bfc1a4bc117d27bea
Sebastien <sebastien@sebs-studio.com>
2013-06-07 23:01:25 +0200

    Version 2.1 release

--- woocommerce-photos-product-tab.php
+++ woocommerce-photos-product-tab.php
@@ -1,40 +1,18 @@
 <?php
 /*
-Plugin Name: WooCommerce Photos Product Tab
-Plugin URI: http://www.sebs-studio.com/wp-plugins/woocommerce-photos-product-tab/
-Description: Extends WooCommerce to allow you to display all images attached to a Product in a new tab on the single product page.
-Version: 1.0
-Author: Sebs Studio (Sebastien)
-Author URI: http://www.sebs-studio.com
-License: GPL2
-*/
+ * Plugin Name: WooCommerce Photos Product Tab
+ * Plugin URI: http://www.sebs-studio.com/wp-plugins/woocommerce-photos-product-tab/
+ * Description: Extends WooCommerce to allow you to display all images attached to a product in a new tab on the single product page.
+ * Version: 2.1
+ * Author: Sebs Studio (Sebastien)
+ * Author URI: http://www.sebs-studio.com
+ *
+ * Text Domain: wc_photos_product_tab
+ * Domain Path: /lang/
+ *
+ */
 
-/*
-	Copyright (C) 2012  Sebastien (email : sebastien@sebs-studio.com)
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-// Plugin Name.
-define('wc_photos_tab_plugin_name', 'WooCommerce Photos Product Tab');
-
-// Plugin version.
-define('wc_photos_tab_plugin_version', '1.0');
-
-// Exit if accessed directly
-if(!defined('ABSPATH')) exit;
+if(!defined('ABSPATH')) exit; // Exit if accessed directly
 
 // Required minimum version of WordPress.
 if(!function_exists('woo_photos_tab_min_required')){
@@ -55,6 +33,12 @@ if(!function_exists('woo_photos_tab_min_required')){
 
 // Checks if the WooCommerce plugins is installed and active.
 if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))){
+
+	/* Localisation */
+	$locale = apply_filters('plugin_locale', get_locale(), 'woocommerce-photos-product-tab');
+	load_textdomain('wc_photos_product_tab', WP_PLUGIN_DIR."/".plugin_basename(dirname(__FILE__)).'/lang/wc_photos_product_tab-'.$locale.'.mo');
+	load_plugin_textdomain('wc_photos_product_tab', false, dirname(plugin_basename(__FILE__)).'/lang/');
+
 	if(!class_exists('WooCommerce_Photo_Product_Tab')){
 		class WooCommerce_Photo_Product_Tab{
 
@@ -108,8 +92,7 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 									'type' => 'sectionend',
 									'id' => 'photos_product_tab'
 								),
-							);
-
+				);
 			}
 
 			/**
@@ -119,8 +102,15 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 				// backend stuff
 				add_filter('plugin_row_meta', array(&$this, 'add_support_link'), 10, 2);
 				// frontend stuff
-				add_action('woocommerce_product_tabs', array(&$this, 'photos_product_tabs'), 25.5);
-				add_action('woocommerce_product_tab_panels', array(&$this, 'photos_product_tabs_panel'), 25.5);
+				if(version_compare(WOOCOMMERCE_VERSION, "2.0", '>=')){
+					// WC >= 2.0
+					add_filter('woocommerce_product_tabs', array($this, 'photos_product_tabs_two'));
+				}
+				else{
+					add_action('woocommerce_product_tabs', array(&$this, 'photos_product_tabs'), 25.5);
+					add_action('woocommerce_product_tab_panels', array(&$this, 'photos_product_tabs_panel'), 25.5);
+				}
+				add_action('wp_enqueue_scripts', array(&$this, 'photos_tab_front_style'));
 				// Settings
 				add_action('woocommerce_settings_catalog_options_after', array(&$this, 'photo_tab_admin_settings'));
 				add_action('woocommerce_update_options_catalog', array(&$this, 'save_photo_tab_admin_settings'));
@@ -130,47 +120,97 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 			}
 
 			/**
-			 * Add donation link to plugin page.
+			 * Add links to plugin page.
 			 */
 			public function add_support_link($links, $file){
 				if(!current_user_can('install_plugins')){
 					return $links;
 				}
 				if($file == WooCommerce_Photo_Product_Tab::$plugin_basefile){
+					$links[] = '<a href="http://www.sebs-studio.com/donation/" target="_blank">'.__('Donate', 'wc_photos_product_tab').'</a>';
 					$links[] = '<a href="http://www.sebs-studio.com/wp-plugins/woocommerce-extensions/" target="_blank">'.__('More WooCommerce Extensions', 'wc_photos_product_tab').'</a>';
 				}
 				return $links;
 			}
 
 			/**
-			 * Write the photos tab on the product view page.
+			 * Add stylesheet to the front for the tab content.
+			 *
+			 * Added since version 2.0
+			 */
+			function photos_tab_front_style(){
+				wp_enqueue_style('woocommerce-photos-product-tab', plugins_url('/assets/css/woocommerce-photos-product-tab.css', __FILE__));
+			}
+
+			/**
+			 * Write the photos tab on the product view page for WC 2.0+.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function photos_product_tabs_two($tabs){
+				global $post, $wpdb;
+
+				/**
+				 * Checks if any photos are attached to the product
+				 * and the tab has not been disabled.
+				 */
+				$countPhotos = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->posts." WHERE `post_type`='attachment' AND `post_parent`='".$post->ID."'");
+
+				if($countPhotos > 0 && get_post_meta($post->ID, 'woocommerce_disable_product_photos', true) != 'yes'){
+
+					$tabs['photos'] = array(
+										'title'    => __('Photos', 'wc_photos_product_tab'),
+										'priority' => 25.5,
+										'callback' => array($this, 'photos_product_tabs_panel_content')
+					);
+
+				}
+				return $tabs;
+			}
+
+			/**
+			 * Write the photo tab on the product view page for WC 1.6.6 and below.
+			 * In WooCommerce these are handled by templates.
 			 */
 			public function photos_product_tabs(){
 				global $post, $wpdb;
 
-			/**
-			 * Checks if any photos are attached to the product.
-			 */
+				/**
+				 * Checks if any photos are attached to the product
+				 * and the tab has not been disabled.
+				 */
 				$countPhotos = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->posts." WHERE `post_type`='attachment' AND `post_parent`='".$post->ID."'");
 
 				if($countPhotos > 0 && get_post_meta($post->ID, 'woocommerce_disable_product_photos', true) != 'yes'){
-					echo "<li class=\"gallery\"><a href=\"#photos-tab\">".__('Photos', 'wc_photos_product_tab')."</a></li>";
+					echo "<li class=\"gallery\"><a href=\"#tab-photos\">".__('Photos', 'wc_photos_product_tab')."</a></li>";
 				}
 			}
 
 			/**
-			 * Write the photos tab panel on the product view page.
+			 * Write the video tab panel on the product view page WC 2.0+.
+			 * In WooCommerce these are handled by templates.
+			 */
+			public function photos_product_tabs_panel_content(){
+				global $post, $wpdb;
+
+				$content = $this->photos_product_tabs_panel();
+
+				return $content;
+			}
+
+			/**
+			 * Write the photo tab panel on the product view page for WC 1.6.6 and below.
+			 * In WooCommerce these are handled by templates.
 			 */
 			public function photos_product_tabs_panel(){
 				global $post, $wpdb;
 
-			/**
-			 * Checks if any photos are attached to the product.
-			 */
+				/**
+				 * Checks if any photos are attached to the product.
+				 */
 				$countPhotos = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->posts." WHERE `post_type`='attachment' AND `post_parent`='".$post->ID."'");
 
 				if($countPhotos > 0 && get_post_meta($post->ID, 'woocommerce_disable_product_photos', true) != 'yes'){
-					echo '<div class="panel entry-content" id="photos-tab">';
+					if(version_compare(WOOCOMMERCE_VERSION, "2.0", '<')){ echo '<div class="panel entry-content" id="tab-photos">'; }
 					echo '<h2>'.__('Photos', 'wc_photos_product_tab').'</h2>';
 					$argsThumb = array(
 						'order'			 => 'ASC',
@@ -182,42 +222,45 @@ if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_o
 					);
 					$attachments = get_posts($argsThumb);
 					if($attachments){
-					  echo '<ul style="list-style:none;">';
+					  echo '<ul>';
 						foreach($attachments as $attachment){
 							$photo_attr = array(
 											'class'	=> "product-photo photo-attachment-".$attachment->ID."",
 											'alt'   => trim( strip_tags( get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ) ) ),
 							);
-							echo '<li style="float:left;">';
-							if(get_option('woocommerce_product_photo_tab_lightbox') == 'yes'){ echo '<a href="'.wp_get_attachment_url($attachment->ID).'" rel="thumbnails" class="zoom">'; }
+							echo '<li>';
+							$woo_light = get_option('woocommerce_enable_lightbox');
+							$photo_light = get_option('woocommerce_product_photo_tab_lightbox');
+							if($woo_light == 'yes' && $photo_light == 'yes'){ echo '<a href="'.wp_get_attachment_url($attachment->ID).'" rel="thumbnails" class="zoom">'; }
 							echo wp_get_attachment_image($attachment->ID, get_option('woocommerce_product_photo_tab_size'), false, $photo_attr);
-						  if(get_option('woocommerce_product_photo_tab_lightbox') == 'yes'){ echo '</a>'; }
+							if($woo_light == 'yes' && $photo_light == 'yes'){ echo '</a>'; }
 							echo '</li>';
 						}
 					  echo '</ul>';
 					}
-					echo '</div>';
+					if(version_compare(WOOCOMMERCE_VERSION, "2.0", '<')){ echo '</div>'; }
 				}
 			}
 
 			// Adds a few settings to control the photos in the tab.
-			function photo_tab_admin_settings() {
+			function photo_tab_admin_settings(){
 				global $settings;
-				woocommerce_admin_fields( $this->settings );
+
+				woocommerce_admin_fields($this->settings);
 			}
 
-			function save_photo_tab_admin_settings() {
-				woocommerce_update_options( $this->settings );
+			function save_photo_tab_admin_settings(){
+				woocommerce_update_options($this->settings);
 			}
 
 			// Adds the option to disable the photo tab on the product page.
-			function write_photo_tab_panel() {
+			function write_photo_tab_panel(){
 		    	echo '<div class="options_group">';
 		    	woocommerce_wp_checkbox( array( 'id' => 'woocommerce_disable_product_photos', 'label' => __('Disable photos tab?', 'wc_photos_product_tab') ) );
 		  		echo '</div>';
 		    }
 		    
-		    function write_photo_tab_panel_save( $post_id ) {
+		    function write_photo_tab_panel_save($post_id){
 		    	$woocommerce_disable_product_photos = isset($_POST['woocommerce_disable_product_photos']) ? 'yes' : 'no';
 		    	update_post_meta($post_id, 'woocommerce_disable_product_photos', $woocommerce_disable_product_photos);
 		    }
@@ -234,7 +277,7 @@ else{
 	function wc_photos_tab_error_notice(){
 		global $current_screen;
 		if($current_screen->parent_base == 'plugins'){
-			echo '<div class="error"><p>'.__(wc_photos_tab_plugin_name.' requires <a href="http://www.woothemes.com/woocommerce/" target="_blank">WooCommerce</a> to be activated in order to work. Please install and activate <a href="'.admin_url('plugin-install.php?tab=search&type=term&s=WooCommerce').'" target="_blank">WooCommerce</a> first.').'</p></div>';
+			echo '<div class="error"><p>WooCommerce Photos Product Tab '.__('requires <a href="http://www.woothemes.com/woocommerce/" target="_blank">WooCommerce</a> to be activated in order to work. Please install and activate <a href="'.admin_url('plugin-install.php?tab=search&type=term&s=WooCommerce').'" target="_blank">WooCommerce</a> first.', 'wc_photos_product_tab').'</p></div>';
 		}
 	}
 }
